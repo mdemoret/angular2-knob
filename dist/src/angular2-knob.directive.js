446355909c80f31425e8d83f7846c1907958bfcd
idanb11 <idanb11@gmail.com>
2017-11-29 21:34:59 +0530

    rebuilding

--- dist/src/angular2-knob.directive.js
+++ dist/src/angular2-knob.directive.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var d3 = require("d3");
@@ -469,21 +472,26 @@ var Ng2KnobDirective = /** @class */ (function () {
         }
     };
     __decorate([
-        core_1.Input()
+        core_1.Input(),
+        __metadata("design:type", Number)
     ], Ng2KnobDirective.prototype, "value", void 0);
     __decorate([
-        core_1.Input()
+        core_1.Input(),
+        __metadata("design:type", Number)
     ], Ng2KnobDirective.prototype, "label", void 0);
     __decorate([
-        core_1.Input()
+        core_1.Input(),
+        __metadata("design:type", Object)
     ], Ng2KnobDirective.prototype, "options", void 0);
     __decorate([
-        core_1.Output()
+        core_1.Output(),
+        __metadata("design:type", Object)
     ], Ng2KnobDirective.prototype, "valueChange", void 0);
     Ng2KnobDirective = __decorate([
         core_1.Directive({
             selector: '[ui-knob]'
-        })
+        }),
+        __metadata("design:paramtypes", [core_1.ElementRef])
     ], Ng2KnobDirective);
     return Ng2KnobDirective;
 }());
